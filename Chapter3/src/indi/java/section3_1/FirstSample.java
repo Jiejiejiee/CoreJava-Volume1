6ccc4bab51ea73ceb396600567c6097c7786fc2a
Jiejiejiee <jiejiejiee77@gmail.com>
2024-06-03 22:19:51 +0800

    20240603

---
+++ Chapter3/src/indi/java/section3_1/FirstSample.java
@@ -0,0 +1,24 @@
+package indi.java.section3_1;
+
+/*
+
+    FirstSample.java源代码文件经过编译后会变成类字节码文件FirstSample.class
+        该字节码文件与源代码文件存储在同一个目录下
+    运行这个已编译的程序java FirstSample
+
+    Java ClassName
+
+    类的声名格式：
+        public class ClassName {
+            public static void main(String[] args) {
+                program statement
+                }
+        }
+
+ */
+
+public class FirstSample {
+    public static void main(String[] args) {
+        System.out.println("We Will not use 'Hello World!'");
+    }
+}
---
+++ Chapter3/src/indi/java/section3_2/Annotation.java
@@ -0,0 +1,21 @@
+package indi.java.section3_2;
+
+//注释方式1
+
+/*
+注释方式2
+ */
+
+/**
+ * 注释方式3
+ */
+
+public class Annotation {
+    public static void main(String[] args) {
+        System.out.println("Java 支持三种注释方式：\n" +
+                "单行注释\n" +
+                "多行注释\n" +
+                "文档注释\n" +
+                "前两种分别是 // 和 /* */，第三种被称作文档注释，它以 /** 开始，以 */ 结束。");
+    }
+}
